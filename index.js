1ac6db405ad65da182b58050a6bd898962782adf
keenwon <semanwmj@gmail.com>
2018-12-22 20:43:33 +0800

    使用 elint 校验

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
 /* eslint-disable no-extra-parens, max-params */
 
-const stringWidth = require('string-width');
+const stringWidth = require('string-width')
 
 // "开"标点
 let openPunctuations = [
@@ -11,10 +11,10 @@ let openPunctuations = [
 
   // 英文
   '(', '[', '{'
-];
+]
 
-function isOpenPunctuation(char) {
-  return openPunctuations.includes(char);
+function isOpenPunctuation (char) {
+  return openPunctuations.includes(char)
 }
 
 // "闭"标点
@@ -25,10 +25,10 @@ let closePunctuations = [
 
   // 英文
   '.', '?', '!', ',', ';', ')', ']', '}'
-];
+]
 
-function isClosePunctuation(char) {
-  return closePunctuations.includes(char);
+function isClosePunctuation (char) {
+  return closePunctuations.includes(char)
 }
 
 // "其他"标点
@@ -39,86 +39,86 @@ let otherPunctuations = [
   // 英文
   // eslint-disable-next-line quotes
   ':', '-', '–', '—', '"', "'"
-];
+]
 
-function isPunctuation(char) {
-  return openPunctuations.includes(char)
-    || closePunctuations.includes(char)
-    || otherPunctuations.includes(char);
+function isPunctuation (char) {
+  return openPunctuations.includes(char) ||
+    closePunctuations.includes(char) ||
+    otherPunctuations.includes(char)
 }
 
 /**
  * 中文
  */
-function isFullWidthChar(char) {
-  return /[\u4e00-\u9fa5]/.test(char);
+function isFullWidthChar (char) {
+  return /[\u4e00-\u9fa5]/.test(char)
 }
 
 /**
  * 获取下一个分割点
  */
-function getNextBreakPoint(str, width, from, lastIndex) {
-  let idealIndex = from;
-  let length = str.length;
-  let subWidth = 0;
+function getNextBreakPoint (str, width, from, lastIndex) {
+  let idealIndex = from
+  let length = str.length
+  let subWidth = 0
 
   // 获取下一个理论上的分割点的索引
   do {
-    idealIndex++;
-    subWidth = stringWidth(str.slice(from, idealIndex));
+    idealIndex++
+    subWidth = stringWidth(str.slice(from, idealIndex))
   } while ((subWidth <= width) && (idealIndex <= length))
 
   // 获取下一个实际分割点（只退不进）
-  let index = idealIndex - 1;
+  let index = idealIndex - 1
 
   if (index >= length) {
-    return length;
+    return length
   }
 
   while (index > lastIndex) {
-    let preValue = str.charAt(index - 1);
-    let value = str.charAt(index);
-    let canBreak = preValue === ' ' || value === ' '
-      || (isFullWidthChar(value) && isFullWidthChar(preValue))
-      || (isClosePunctuation(preValue) && !isPunctuation(value))
-      || (!isPunctuation(preValue) && isOpenPunctuation(value))
-      || (isClosePunctuation(preValue) && isOpenPunctuation(value));
+    let preValue = str.charAt(index - 1)
+    let value = str.charAt(index)
+    let canBreak = preValue === ' ' || value === ' ' ||
+      (isFullWidthChar(value) && isFullWidthChar(preValue)) ||
+      (isClosePunctuation(preValue) && !isPunctuation(value)) ||
+      (!isPunctuation(preValue) && isOpenPunctuation(value)) ||
+      (isClosePunctuation(preValue) && isOpenPunctuation(value))
 
     if (canBreak) {
-      break;
+      break
     } else {
-      index--;
+      index--
     }
   }
 
   // 强制截断
   if (index <= lastIndex) {
-    index = idealIndex;
+    index = idealIndex
   }
 
-  return index;
+  return index
 }
 
-function breaker(str, width) {
+function breaker (str, width) {
   if (width < 2) {
-    throw new Error('Width must be greater than 2');
+    throw new Error('Width must be greater than 2')
   }
 
-  let length = str.length;
-  let index = 0;
-  let breakPoint = 0;
-  let line, lines = [];
+  let length = str.length
+  let index = 0
+  let breakPoint = 0
+  let line; let lines = []
 
   while (index < length) {
-    breakPoint = getNextBreakPoint(str, width, index, breakPoint);
-    line = str.slice(index, breakPoint).trim();
+    breakPoint = getNextBreakPoint(str, width, index, breakPoint)
+    line = str.slice(index, breakPoint).trim()
 
-    lines.push(line);
+    lines.push(line)
 
-    index = breakPoint;
+    index = breakPoint
   }
 
-  return lines;
+  return lines
 }
 
-module.exports = breaker;
+module.exports = breaker
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,17 +1,14 @@
-'use strict';
+'use strict'
 
 /* eslint-disable max-len, max-nested-callbacks */
 
-const stringWidth = require('string-width');
-const stringBreak = require('../index.js');
+const stringWidth = require('string-width')
+const stringBreak = require('../index.js')
 
-const mocha = require('mocha');
-const chai = require('chai');
-chai.should();
+const chai = require('chai')
+chai.should()
 
 describe('String Break Test', function () {
-
-
   const strObjs = [
     {
       title: '英文',
@@ -25,15 +22,15 @@ describe('String Break Test', function () {
       title: '英文 + 中文',
       text: '《Better Me》 远处海港传来阵阵船笛 我一直飘零到被你拣起 如今望著反映窗户玻璃 有个我陌生又熟悉 I can Smile a little more Sing a little more Feel a little more 全因为你 说好了要为幸福 一天天地练习 练习 Laugh a little more Love myself a little more 要学会更加善待我自己 为你我变成了 Better me 甚麼距离都不算是真的分离 想念和默契能代替一切言语 有一天生命会老去 还好谢谢有你 在你眼中 I see the better in me Coz I can Smile a little more Sing a little more Feel a little more 全因为你 说好了要为幸福 一天天地练习 练习 Laugh a little more Love myself a little more 要学会更加善待我自己 为你我变成了 Better me 就是那麼神奇 从前的错都有意义 教我抛开所有猜疑 也许 我也美丽 值得一个奇迹 我的眼泪会坠落 绝不是因为懦弱 而是感谢天让我遇见你 不然今天就不能 如此地有勇气 Now I promise to you And I can swear to you 为你我 一定加倍 爱护我自己 做一个值得你 骄傲的 Better me 一个值得你爱的Better me'
     }
-  ];
+  ]
 
   for (let width = 20; width <= 21; width++) {
     strObjs.forEach(({ title, text }) => {
       it(`${title} width: ${width}`, () => {
         return stringBreak(text, width).map(line => {
-          return stringWidth(line).should.most(width);
-        });
-      });
-    });
+          return stringWidth(line).should.most(width)
+        })
+      })
+    })
   }
-});
+})
