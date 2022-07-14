7cf5325fde71650885de77fa6008d16409cac9ce
keenwon <semanwmj@gmail.com>
2022-07-14 13:15:28 +0800

    feat: 升级 elint

--- index.js
+++ index.js
@@ -7,53 +7,89 @@ const stringWidth = require('string-width')
 // "开"标点
 const openPunctuations = [
   // 中文
-  '“', '‘', '（', '《', '〈', '〔', '【',
+  '“',
+  '‘',
+  '（',
+  '《',
+  '〈',
+  '〔',
+  '【',
 
   // 英文
-  '(', '[', '{'
+  '(',
+  '[',
+  '{',
 ]
 
-function isOpenPunctuation (char) {
+function isOpenPunctuation(char) {
   return openPunctuations.includes(char)
 }
 
 // "闭"标点
 const closePunctuations = [
   // 中文
-  '。', '？', '！', '，', '、', '；', '”',
-  '’', '）', '》', '〉', '〕', '】',
+  '。',
+  '？',
+  '！',
+  '，',
+  '、',
+  '；',
+  '”',
+  '’',
+  '）',
+  '》',
+  '〉',
+  '〕',
+  '】',
 
   // 英文
-  '.', '?', '!', ',', ';', ')', ']', '}'
+  '.',
+  '?',
+  '!',
+  ',',
+  ';',
+  ')',
+  ']',
+  '}',
 ]
 
-function isClosePunctuation (char) {
+function isClosePunctuation(char) {
   return closePunctuations.includes(char)
 }
 
 // "其他"标点
 const otherPunctuations = [
   // 中文
-  '：', '─', '…', '·',
+  '：',
+  '─',
+  '…',
+  '·',
 
   // 英文
   // eslint-disable-next-line quotes
-  ':', '-', '–', '—', '"', "'"
+  ':',
+  '-',
+  '–',
+  '—',
+  '"',
+  "'",
 ]
 
-function isPunctuation (char) {
-  return openPunctuations.includes(char) ||
+function isPunctuation(char) {
+  return (
+    openPunctuations.includes(char) ||
     closePunctuations.includes(char) ||
     otherPunctuations.includes(char)
+  )
 }
 
 // 中文
-function isFullWidthChar (char) {
+function isFullWidthChar(char) {
   return /[\u4e00-\u9fa5]/.test(char)
 }
 
 // 获取下一个分割点
-function getNextBreakPoint (str, width, from, lastIndex) {
+function getNextBreakPoint(str, width, from, lastIndex) {
   let idealIndex = from
   const length = str.length
   let subWidth = 0
@@ -62,7 +98,7 @@ function getNextBreakPoint (str, width, from, lastIndex) {
   do {
     idealIndex++
     subWidth = stringWidth(str.slice(from, idealIndex))
-  } while ((subWidth <= width) && (idealIndex <= length))
+  } while (subWidth <= width && idealIndex <= length)
 
   // 获取下一个实际分割点（只退不进）
   let index = idealIndex - 1
@@ -74,7 +110,9 @@ function getNextBreakPoint (str, width, from, lastIndex) {
   while (index > lastIndex) {
     const preValue = str.charAt(index - 1)
     const value = str.charAt(index)
-    const canBreak = preValue === ' ' || value === ' ' ||
+    const canBreak =
+      preValue === ' ' ||
+      value === ' ' ||
       (isFullWidthChar(value) && isFullWidthChar(preValue)) ||
       (isClosePunctuation(preValue) && !isPunctuation(value)) ||
       (!isPunctuation(preValue) && isOpenPunctuation(value)) ||
@@ -95,7 +133,7 @@ function getNextBreakPoint (str, width, from, lastIndex) {
   return index
 }
 
-function breaker (str, width) {
+function breaker(str, width) {
   if (width < 2) {
     throw new Error('Width must be greater than 2')
   }
@@ -103,7 +141,8 @@ function breaker (str, width) {
   const length = str.length
   let index = 0
   let breakPoint = 0
-  let line; const lines = []
+  let line
+  const lines = []
 
   while (index < length) {
     breakPoint = getNextBreakPoint(str, width, index, breakPoint)
